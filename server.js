22987f7a2454054833d0464862628c8ce342672b
vapvin <tpehdj12@daum.net>
2020-04-06 18:28:04 +0900

    Server Js Created

--- server.js
+++ server.js
@@ -5,4 +5,48 @@ const http = require('http');
 const https = require('https');
 const sio = require('socket.io')
 const favicon = require('serve-favicon');
+const compression = require('compression');
+
+const app = express(),
+    port = process.env.PORT || 3000,
+    server = process.env.NODE_ENV == 'production' ?
+        https.createServer(options, app).listen(port) : http.createServer(app).listen(port),
+    io = sio(server);
+
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use((req, res) => res.sendFile(__dirname + '/dist/index.html'));
+app.use(favicon('./dist/favicon.ico'));
+app.disable('x-powered-by');
+
+io.sockets.on('connection', socket => {
+    let room = '';
+    socket.on('message', message => socket.broadcast.to(room).emit('message', message));
+    socket.on('find', () => {
+        const url = socket.request.headers.referer.split('/');
+        room = url[url.length - 1];
+        const sr = io.sockets.adapter.rooms[room];
+        if(sr === undefined){
+            socket.join(room);
+            socket.emit('create');
+        } else if(sr.length === 1){
+            socket.emit('join');
+        } else {
+            socket.emit('full', room);
+        }
+    });
+    socket.on('auth', data => {
+        data.sid = socket.id;
+        socket.broadcast.to(room).emit('approve', data);
+    });
+    socket.on('accept', id => {
+        io.sockets.connected[id].join(room);
+        io.in(room).emit('bridge');
+    });
+    socket.on('reject', () => socket.emit('full'));
+    socket.on('leave', () => {
+        socket.broadcast.to(room).emit('hangup');
+        socket.leave(room);
+    });
+});
 
